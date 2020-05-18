c7e506fd7c947a67068906fc4f791125d6847482
hansknowyou <yuelun123321@hotmail.com>
2020-05-18 13:05:01 -0400

    Updated transactions & readMe

--- scripts/index.ts
+++ scripts/index.ts
@@ -10,8 +10,8 @@ import { IProvice } from '../types/province'
 
 const parser = csv({ separator: '\t' })
 const unitCost = 2.6
-const fromDate = '2020-02-01'
-const toDate = '2020-02-29'
+const fromDate = '2020-04-01'
+const toDate = '2020-04-30'
 
 const importPath = path.join(
   __dirname,
